7f70af8d9dbfcd159d82b8c6bf8d15d1620d3a1c
bgutz001 <bgutz001@ucr.edu>
2015-12-20 16:45:47 -0800

    Initial messing around. Able to get Longitude and Latitude data

---
+++ RealEXP/app/src/main/java/realexp/realexp/Location.java
@@ -0,0 +1,66 @@
+package realexp.realexp;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.location.*;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.jar.Manifest;
+
+public class Location extends AppCompatActivity {
+
+    android.location.LocationManager locationM;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+
+        locationM = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_location, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void btnLocation_Click(View v) {
+        try {
+            android.location.Location l = locationM.getLastKnownLocation(locationM.GPS_PROVIDER);
+            double lat = l.getLatitude();
+            double lon = l.getLongitude();
+            TextView latitude = (TextView) findViewById(R.id.txtLatiude);
+            TextView longitude = (TextView) findViewById(R.id.txtLongitude);
+            latitude.setText(Double.toString(lat));
+            longitude.setText(Double.toString(lon));
+
+
+        } catch (SecurityException e) {
+            Log.e("PERMISSION", "ACCESS_FINE_LOCATION");
+        }
+    }
+}
