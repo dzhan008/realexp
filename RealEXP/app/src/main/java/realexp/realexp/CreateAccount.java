798be441b8985198cc0ee494dd7d5fe25be66220
dzhan008 <dzhan008@ucr.edu>
2016-05-01 12:41:42 -0700

    Attempted Account Creation/Login Screen

---
+++ RealEXP/app/src/main/java/realexp/realexp/CreateAccount.java
@@ -0,0 +1,13 @@
+package realexp.realexp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
