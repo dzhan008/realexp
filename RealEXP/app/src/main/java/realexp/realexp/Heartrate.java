3eb869672f45cee86c1712ada480ef1264f8d353
bgutz001 <bgutz001@ucr.edu>
2016-05-22 11:51:27 -0700

    Prototype of the heart rate monitor works

--- RealEXP/app/src/main/java/realexp/realexp/Heartrate.java
+++ RealEXP/app/src/main/java/realexp/realexp/Heartrate.java
@@ -2,6 +2,7 @@ package realexp.realexp;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -14,35 +15,87 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class Heartrate extends AppCompatActivity implements SensorEventListener {
 
+    TextView txtNoSensor;
+    TextView txtHeartRate;
+
     SensorManager sensorM;
     Sensor sensorHeartrate;
-    Camera camera;
-    SurfaceView sView;
 
-    public enum COLOR {
-        GREEN, RED
-    }
-    COLOR color = COLOR.RED;
 
-    // Sensor
+
+
+    float time1;
+    float time2;
+    List<Float> BPMS;
     @Override
     public void onSensorChanged(SensorEvent event) {
-        Log.i("Beats per min", Float.toString(event.values[0]));
+        if (event.accuracy == SensorManager.SENSOR_STATUS_ACCURACY_HIGH ||
+                event.accuracy == SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM) {
+            if (time1 == 0) time1 = event.timestamp;
+            time2 = event.timestamp;
+
+            Log.i("Beats per min", Float.toString(event.values[0]));
+            txtHeartRate.setText(Float.toString(event.values[0]));
+            BPMS.add(event.values[0]);
+
+        /*
+        Log.i("Time", Float.toString(time2 - time1));
+        if ((time2 - time1) >= 15) {
+            Log.i("Listner", "Unregister");
+            sensorM.unregisterListener(this, sensorHeartrate);
+        }
+        */
+        }
+
     }
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         if (accuracy == SensorManager.SENSOR_STATUS_NO_CONTACT ||
                 accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE) {
-
+                // TODO tell user to place finger on sensor
+            txtHeartRate.setText("Please keep your finger still on the sensor");
+            time1 = 0;
+            BPMS.clear();
         }
+    }
+
+    public void btnMeasureHeartRate_click(View v) {
+        time1 = 0;
+        time2 = 0;
+        BPMS.clear();
+        List<Sensor> sensors = sensorM.getSensorList(Sensor.TYPE_HEART_RATE);
+
+        if (sensors == null) {
+            //TODO
+            txtNoSensor.setVisibility(View.VISIBLE);
+        }
+        else {
+            txtNoSensor.setVisibility(View.INVISIBLE);
+        }
+
+        for (Sensor s : sensors) {
+            //TODO Decide which one to use
+            Log.i("Heart Rate Sensor", s.getName());
+            sensorHeartrate = s;
+        }
+
+        if (!sensorM.registerListener(this, sensorHeartrate, SensorManager.SENSOR_DELAY_FASTEST)) {
+            Log.e("Heart Rate Sensor", "Failed to register listener");
+            txtHeartRate.setText("Could not connect to device.");
+            return;
+        }
+
+        txtHeartRate.setText("Waiting to measure heart rate.");
     }
 
     // Activity
@@ -51,122 +104,38 @@ public class Heartrate extends AppCompatActivity implements SensorEventListener 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heartrate);
 
+        txtNoSensor = (TextView)findViewById(R.id.txtError);
+        txtHeartRate = (TextView)findViewById(R.id.txtHeartRate);
+
         sensorM = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensorHeartrate = sensorM.getDefaultSensor(Sensor.TYPE_HEART_RATE);
-        sView = (SurfaceView) findViewById(R.id.surfaceView);
-
-        getCamera();
-
-        /*
-        TextView t = (TextView) findViewById(R.id.txtSensor);
-        if (sensorHeartrate != null) { // a sensor exists
-            sensorM.registerListener(this, sensorHeartrate, SensorManager.SENSOR_DELAY_FASTEST);
-            Log.i("Heart Sensor", sensorHeartrate.getName());
-            t.setText("Sensor found"); // TODO Erase This
-        }
-        else {  // start the camera sensor
-            t.setText("No Sensor Found"); // TODO Erase This
-            if (this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-                // this device has a camera
+        BPMS = new ArrayList<>();
 
 
 
 
 
-            } else {
-                // no camera on this device
-                //TODO
-            }
-        }
-        */
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        camera = Camera.open();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        camera.release();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        camera.stopPreview();
-        camera.release();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        camera.release();
     }
-
-    public void getCamera() {
-        camera = camera.open();
-        SurfaceHolder sHolder = sView.getHolder();
-        sHolder.addCallback(sHolderCallback);
-        sHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-
-    }
-
-    private Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
-
-        @Override
-        public void onPreviewFrame(byte[] data, Camera camera) {
-            if (data == null) return;
-            Camera.Size size = camera.getParameters().getPreviewSize();
-            if (size == null) return;
-
-
-        }
-    };
-
-    private SurfaceHolder.Callback sHolderCallback = new SurfaceHolder.Callback() {
-        @Override
-        public void surfaceCreated(SurfaceHolder holder) {
-            try {
-                camera.setDisplayOrientation(90);
-                camera.setPreviewDisplay(holder);
-                camera.setPreviewCallback(previewCallback);
-                camera.startPreview();
-            } catch (IOException e) {
-                e.printStackTrace();
-                Log.e("SurfaceHolderCallback", "Error in setPreview");
-            }
-        }
-        @Override
-        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-            Camera.Parameters parameters = camera.getParameters();
-            List<Camera.Size> previewSizes = parameters.getSupportedPreviewSizes();
-            Camera.Size newSize = parameters.getPreviewSize();
-            for (Camera.Size size : previewSizes) {
-                if (size.width <= width && size.height < height) {
-                    newSize = size;
-                }
-            }
-            //parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-            //parameters.setPreviewFormat(format);
-            parameters.setPreviewSize(newSize.width, newSize.height);
-
-            try {
-                camera.setParameters(parameters);
-            } catch (Exception e) {
-                e.printStackTrace();
-                Log.e("Camera Paramters", "Bad parameters");
-            }
-            camera.startPreview();
-        }
-        @Override
-        public void surfaceDestroyed(SurfaceHolder holder) {
-            camera.stopPreview();
-        }
-    };
 
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
